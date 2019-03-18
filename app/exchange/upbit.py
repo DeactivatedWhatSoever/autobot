bb8a9186635169c50a7750ade070969a928093d6
RobinRheem <roborheems@gmail.com>
2019-03-19 00:11:57 +0900

    Fix variable mismatch and add datetime

--- app/exchange/upbit.py
+++ app/exchange/upbit.py
@@ -12,16 +12,16 @@ def buy(symbol):
     sell_price = orderbook[0]['orderbook_units'][0]['ask_price']
     unit = float(krw) / float(sell_price)
     #upbit_api.buy_limit_order(symbol, sell_price, unit)
-    print('Balance:', krw, 'Buy Price:', sell_price, 'Shares:', unit)
+    print("Now:", datetime.datetime.now(), 'Balance:', krw, 'Buy Price:', sell_price, 'Shares:', unit)
 
 
 def sell(symbol):
     krw = upbit_api.get_balances()[0][0]['balance']
     orderbook = pyupbit.get_orderbook(symbol)
     buy_price = orderbook[0]['orderbook_units'][0]['bid_price']
-    unit = float(krw) / float(sell_price)
+    unit = float(krw) / float(buy_price)
     #upbit_api.sell_limit_order(symbol, buy_price, unit)
-    print('Balance:', krw, 'Sell Price:', buy_price, 'Shares:', unit)
+    print("Now:", datetime.datetime.now(), 'Balance:', krw, 'Sell Price:', buy_price, 'Shares:', unit)
 
 
 def ohlcv(symbol='KRW-BTC', timeframe='day'):
